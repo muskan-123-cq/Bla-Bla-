096d71171a4544bb904f0ad77430d4de0f5662fc
muskan-123-cq <musk.verma123@gmail.com>
2025-09-24 22:17:34 +0530

    fileDB.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect(`${process.env.MONGO_URI}/bla_bla_db`)
         console.log('MongoDB connected successfully.');
     } catch (err) {
         console.error('MongoDB connection failed:', err.message);
